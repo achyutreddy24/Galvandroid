81c2c3fee22e2f35a4150f469bc4cedfddd361d7
Achyut Reddy <achyutreddy24@gmail.com>
2016-03-25 13:00:24 -0400

    Add basic ui, immersive_sticky mode for galvanic cell activity

--- app/src/main/java/com/galvandroid/galvandroid/MainActivity.java
+++ app/src/main/java/com/galvandroid/galvandroid/MainActivity.java
@@ -1,13 +1,31 @@
 package com.galvandroid.galvandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button galvanicCell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        galvanicCell = (Button) findViewById(R.id.galvanic_cell);
+
+        galvanicCell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), GalvanicCellActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
