81dcd7fa7369c9dc82c883353c4f2a255e06eca7
Achyut Reddy <achyutreddy24@gmail.com>
2016-03-26 15:49:26 -0400

    Add more in getNonStandard

--- app/src/main/java/com/galvandroid/galvandroid/Chemistry/Util.java
+++ app/src/main/java/com/galvandroid/galvandroid/Chemistry/Util.java
@@ -3,17 +3,36 @@ package com.galvandroid.galvandroid.Chemistry;
 
 public class Util {
 
-    public static CustomHalfReaction getAnode(CustomHalfReaction a, CustomHalfReaction b) {
+    private static final double faraday = 0.0592;
 
+    public static double getNonStandard(CustomHalfReaction a, CustomHalfReaction b) {
+        double standard;
+        double ared = a.getBaseReaction().getReductionPotential();
+        double bred = b.getBaseReaction().getReductionPotential();
+        int n = lcmcal(a.getBaseReaction().getElectrons(), b.getBaseReaction().getElectrons()); //Gets LCM of the two electron counts
 
-        return a;
+        if(ared >= bred) {
+            standard = ared + (-1 * bred);
+        } else {
+            standard = (-1 * ared) + bred;
+        }
+
+        double q = 1.00;
+        return faraday / n * Math.log(q);
     }
 
-    public static double getnonStandard(CustomHalfReaction a, CustomHalfReaction b){
-        double ared = a.getBaseReaction().getReductionPotential();
-        int n = a.getMultiplier();
-        double q = 1.00;
-        return 0.0592/n*Math.log(q);
+
+    private static int lcmcal(int i, int y) {
+        int n, x, s = 1, t = 1;
+        for (n = 1; ; n++) {
+            s = i * n;
+            for (x = 1; t < s; x++) {
+                t = y * x;
+            }
+            if (s == t)
+                break;
+        }
+        return (s);
     }
 
 }
