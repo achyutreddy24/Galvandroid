29121f792ccf0660599e43d7725e33493feae60e
wc24601 <wchen20111001@gmail.com>
2016-03-26 15:39:32 -0400

    Added methods to Util

--- app/src/main/java/com/galvandroid/galvandroid/Chemistry/Util.java
+++ app/src/main/java/com/galvandroid/galvandroid/Chemistry/Util.java
@@ -3,4 +3,18 @@ package com.galvandroid.galvandroid.Chemistry;
 
 public class Util {
 
+    public static CustomHalfReaction getAnode(CustomHalfReaction a, CustomHalfReaction b) {
+
+
+        return a;
+    }
+
+    public static double getnonStandard(CustomHalfReaction a, CustomHalfReaction b){
+        double ared = a.getBaseReaction().getReductionPotential();
+        int n = a.getMultiplier();
+        double q = 1.0;
+        return 0.0592/n*Math.log(q);
+    }
+
 }
+
